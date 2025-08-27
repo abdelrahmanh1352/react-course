f861e681947961772e24ec1678a561a2dbc10d83
abdelrahmanh1352 <abdelrahmanh1352@gmail.com>
2025-08-27 22:47:37 +0300

    feat: add TabButton component using children prop for composition

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -2,6 +2,7 @@ import { CORE_CONCEPTS } from "./data";
 
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcepts";
+import TabButton from "./components/TabButton";
 
 function App() {
   return (
@@ -20,6 +21,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
