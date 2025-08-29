a6b4110f4cd4258241d018b5450fec2272aa757e
abdelrahmanh1352 <abdelrahmanh1352@gmail.com>
2025-08-29 20:58:23 +0300

    refactor: extract CoreConcepts and Examples into separate components for better structure

--- 03 React Essentials/01-starting-project/src/components/CoreConcepts.jsx
+++ 03 React Essentials/01-starting-project/src/components/CoreConcepts.jsx
@@ -1,9 +1,16 @@
-export default function CoreConcept({ title, description, image }) {
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConcept";
+const CoreConcepts = () => {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+      </ul>
+    </section>
   );
-}
+};
+
+export default CoreConcepts;
