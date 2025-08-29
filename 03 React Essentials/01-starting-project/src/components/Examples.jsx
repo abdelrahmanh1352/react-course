1418f4abf092588ec674b22c86c71f21c96f0902
abdelrahmanh1352 <abdelrahmanh1352@gmail.com>
2025-08-29 21:05:32 +0300

    refactor: use props forwarding with spread operator in Section and TabButton

--- 03 React Essentials/01-starting-project/src/components/Examples.jsx
+++ 03 React Essentials/01-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./TabButton";
+import Section from "./Section";
 
 const Examples = () => {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -12,30 +13,29 @@ const Examples = () => {
 
   console.log("APP COMPONENT EXECUTING");
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
-          onSelect={() => handleSelect("components")}
+          onClick={() => handleSelect("components")}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "jsx"}
-          onSelect={() => handleSelect("jsx")}
+          onClick={() => handleSelect("jsx")}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "props"}
-          onSelect={() => handleSelect("props")}
+          onClick={() => handleSelect("props")}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "state"}
-          onSelect={() => handleSelect("state")}
+          onClick={() => handleSelect("state")}
         >
           State
         </TabButton>
@@ -51,7 +51,7 @@ const Examples = () => {
           </pre>
         </div>
       )}
-    </section>
+    </Section>
   );
 };
 
